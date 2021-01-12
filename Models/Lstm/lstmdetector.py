eec0bcb85c6f9c5ac3f3f92f8b9318ab9ce1dee2
noamarbe <noam.arbel@intel.com>
2021-01-12 10:54:31 +0200

    update lstm uncertainty

--- Models/Lstm/lstmdetector.py
+++ Models/Lstm/lstmdetector.py
@@ -27,7 +27,6 @@ class LstmDetector(AnomalyDetectionModel):
         return model
 
 
-
 class LstmModel(nn.Module):
     def __init__(self, num_features, hidden_layer, batch_size, dropout_p, device, batch_first=True):
         super(LstmModel, self).__init__()
@@ -35,19 +34,22 @@ class LstmModel(nn.Module):
         self.num_features = num_features
         self.hidden_layer = hidden_layer
         self.batch_size = batch_size
-        self.n_layers = 1
+        self.n_layers = 2
         self.device = device
 
         self.dropout = nn.Dropout(dropout_p)
         self.lstm = nn.LSTM(self.num_features,
                             self.hidden_layer,
+                            dropout=dropout_p,
+                            num_layers=self.n_layers,
                             batch_first=batch_first)
         self.linear = nn.Linear(self.hidden_layer, self.num_features)
+        self.relu = nn.ReLU()
 
     def forward(self, input_seq, h, test=None):
         lstm_out, a = self.lstm(input_seq, h)
         lstm_out = lstm_out.contiguous().view(-1, self.hidden_layer)
-        lstm_out = self.dropout(lstm_out)
+        lstm_out = self.dropout(self.relu(lstm_out))
         predictions = self.linear(lstm_out)
         if not test:
             predictions = predictions.view(self.batch_size, -1, self.num_features)
@@ -62,7 +64,7 @@ class LstmModel(nn.Module):
         if not batch_size:
             batch_size = self.batch_size
 
-        hidden = (weight.new(1, batch_size, self.hidden_layer).zero_().to(self.device),
-                  weight.new(1, batch_size, self.hidden_layer).zero_().to(self.device))
+        hidden = (weight.new(self.n_layers, batch_size, self.hidden_layer).zero_().to(self.device),
+                  weight.new(self.n_layers, batch_size, self.hidden_layer).zero_().to(self.device))
 
         return hidden
